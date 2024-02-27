a8c6f3b7e7351f421413c64e49253bfa2bb82213
Ins1der2317 <150137722+Ins1der2317@users.noreply.github.com>
2024-02-27 11:37:46 +0000

    Atualização rotas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PageController;
+use App\Http\Controllers\HomeController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Password;
+
 
 /*
 |--------------------------------------------------------------------------
