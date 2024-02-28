6dca6d6601f076073920bd7d1640d9aee5cbab28
Paulo André Leal <pauloandeleal@gmail.com>
2024-02-28 14:03:29 +0000

    banner

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,15 +1,72 @@
-
 @extends('partials.master')
 @include('partials.navbar')
 @section('title', 'Página Inicial')
+
+@section('moreCSS')
+    <link rel="stylesheet" href="{{ asset('css/main.css') }}">
+@endsection
 
 
 @section('content')
 
 
-<h1>Teste</h1>
-<p>
-    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><p>ola</p>
-</p>
+    <h1>Teste</h1>
+    <p>
+    <p>ola</p>
+    </p>
 
-@endsection
+    <div class="banner">
+        <img src="{{ asset('img/dentist-2589771.jpg') }}" alt="banner">
+    </div>
+
+    <div>
+        <div class="carousel">
+            <button class="carousel__button carousel__button--left is-hidden">
+                <img src="https://www.iconpacks.net/icons/2/free-arrow-left-icon-3099-thumb.png" alt="left" />
+            </button>
+            <div class="carousel__track-container">
+                <ul class="carousel__track">
+                    <li class="carousel__slide current-slide">
+                        <img class="carousel__images"
+                            src="https://images.pexels.com/photos/19682307/pexels-photo-19682307/free-photo-of-pink-flowers-on-a-shrub.jpeg"
+                            alt="background-image" />
+                    </li>
+                    <li class="carousel__slide">
+                        <img class="carousel__images"
+                            src="https://images.pexels.com/photos/17617625/pexels-photo-17617625/free-photo-of-pink-chrysanthemum-flowers.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+                            alt="background-image" />
+                    </li>
+                    <li class="carousel__slide">
+                        <img class="carousel__images"
+                            src="https://images.pexels.com/photos/17617625/pexels-photo-17617625/free-photo-of-pink-chrysanthemum-flowers.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+                            alt="background-image" />
+                    </li>
+                    <li class="carousel__slide">
+                        <img class="carousel__images"
+                            src="https://images.pexels.com/photos/19682307/pexels-photo-19682307/free-photo-of-pink-flowers-on-a-shrub.jpeg"
+                            alt="background-image" />
+                    </li>
+                    <li class="carousel__slide">
+                        <img class="carousel__images"
+                            src="https://images.pexels.com/photos/19598202/pexels-photo-19598202/free-photo-of-pink-hyndrangea-flowers.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+                            alt="background-image" />
+                    </li>
+                </ul>
+            </div>
+            <button class="carousel__button carousel__button--right">
+                <img src="https://www.iconpacks.net/icons/2/free-arrow-left-icon-3099-thumb.png" alt="right" />
+            </button>
+            <div class="carousel__nav">
+                <button class="carousel__indicator current-slide"></button>
+                <button class="carousel__indicator"></button>
+                <button class="carousel__indicator"></button>
+                <button class="carousel__indicator"></button>
+                <button class="carousel__indicator"></button>
+            </div>
+        </div>
+
+    @endsection
+
+    @section('moreScripts')
+        <script src="{{ asset('js/carousel.js') }}"></script>
+    @endsection
--- resources/views/partials/master.blade.php
+++ resources/views/partials/master.blade.php
@@ -7,8 +7,6 @@
     <link rel="shortcut icon" href="{{ asset('img/favicon.png') }}">
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-
-     <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('css/navbar.css') }}">
     <link rel="stylesheet" href="{{ asset('css/footer.css') }}">
     @yield('moreCSS')
