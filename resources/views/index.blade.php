5f61758621f2f0c1a09aae7d920b4add10275b16
Ins1der2317 <150137722+Ins1der2317@users.noreply.github.com>
2024-02-29 11:33:41 +0000

    Index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,63 +9,70 @@
 
 @section('content')
 
-
-
-
-
-
-    <div>
-       <div class="carousel">
-    <button class="carousel__button carousel__button--left is-hidden">
-        <img src="https://www.iconpacks.net/icons/2/free-arrow-left-icon-3099-thumb.png" alt="left" />
-    </button>
-    <div class="carousel__track-container">
-        <ul class="carousel__track">
-            <li class="carousel__slide current-slide">
-                <img class="carousel__images" src="https://images.pexels.com/photos/19682307/pexels-photo-19682307/free-photo-of-pink-flowers-on-a-shrub.jpeg" alt="background-image" />
-            </li>
-                    <li class="carousel__slide">
-                        <img class="carousel__images"
-                            src="https://images.pexels.com/photos/17617625/pexels-photo-17617625/free-photo-of-pink-chrysanthemum-flowers.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
-                            alt="background-image" />
-                    </li>
-                    <li class="carousel__slide">
-                        <img class="carousel__images"
-                            src="https://images.pexels.com/photos/17617625/pexels-photo-17617625/free-photo-of-pink-chrysanthemum-flowers.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
-                            alt="background-image" />
-                    </li>
-                    <li class="carousel__slide">
-                        <img class="carousel__images"
-                            src="https://images.pexels.com/photos/19682307/pexels-photo-19682307/free-photo-of-pink-flowers-on-a-shrub.jpeg"
-                            alt="background-image" />
-                    </li>
-                    <li class="carousel__slide">
-                        <img class="carousel__images"
-                            src="https://images.pexels.com/photos/19598202/pexels-photo-19598202/free-photo-of-pink-hyndrangea-flowers.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
-                            alt="background-image" />
-                    </li>
-                    <button class="carousel__button carousel__button--right">
-                <img src="https://www.iconpacks.net/icons/2/free-arrow-left-icon-3099-thumb.png" alt="right" />
-            </button>
-            <div class="carousel__nav">
-                <button class="carousel__indicator current-slide"></button>
-                <button class="carousel__indicator"></button>
-                <button class="carousel__indicator"></button>
-                <button class="carousel__indicator"></button>
-                <button class="carousel__indicator"></button>
-            </div>
-        </div>
-                </ul>
-
-            </div>
-
-
-        <h1>Teste</h1>
-  {{--
 <div class="banner">
         <img src="{{ asset('img/dentist-2589771.jpg') }}" alt="banner">
     </div>
---}}
+
+
+<div>
+    <h1>Carousel</h1>
+    <div>
+      <div class="carousel">
+        <button class="carousel__button carousel__button--left is-hidden">
+          <img src="https://www.iconpacks.net/icons/2/free-arrow-left-icon-3099-thumb.png" alt="left" />
+        </button>
+        <div class="carousel__track-container">
+          <ul class="carousel__track">
+            <li class="carousel__slide current-slide">
+              <img
+                class="carousel__images"
+                src="https://images.pexels.com/photos/19682307/pexels-photo-19682307/free-photo-of-pink-flowers-on-a-shrub.jpeg"
+                alt="background-image"
+              />
+            </li>
+            <li class="carousel__slide">
+              <img
+                class="carousel__images"
+                src="https://images.pexels.com/photos/17617625/pexels-photo-17617625/free-photo-of-pink-chrysanthemum-flowers.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+                alt="background-image"
+              />
+            </li>
+            <li class="carousel__slide">
+              <img
+                class="carousel__images"
+                src="https://images.pexels.com/photos/17617625/pexels-photo-17617625/free-photo-of-pink-chrysanthemum-flowers.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+                alt="background-image"
+              />
+            </li>
+            <li class="carousel__slide">
+              <img
+                class="carousel__images"
+                src="https://images.pexels.com/photos/19682307/pexels-photo-19682307/free-photo-of-pink-flowers-on-a-shrub.jpeg"
+                alt="background-image"
+              />
+            </li>
+            <li class="carousel__slide">
+              <img
+                class="carousel__images"
+                src="https://images.pexels.com/photos/19598202/pexels-photo-19598202/free-photo-of-pink-hyndrangea-flowers.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+                alt="background-image"
+              />
+            </li>
+          </ul>
+        </div>
+        <button class="carousel__button carousel__button--right">
+          <img src="https://www.iconpacks.net/icons/2/free-arrow-left-icon-3099-thumb.png" alt="right" />
+        </button>
+        <div class="carousel__nav">
+          <button class="carousel__indicator current-slide"></button>
+          <button class="carousel__indicator"></button>
+          <button class="carousel__indicator"></button>
+          <button class="carousel__indicator"></button>
+          <button class="carousel__indicator"></button>
+        </div>
+      </div>
+
+<h1>Teste</h1>
 
     @endsection
 
