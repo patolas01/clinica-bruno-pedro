d3aa5bbbca5bedbfb9885a8ef8e09f0c6efc2c60
Ins1der2317 <150137722+Ins1der2317@users.noreply.github.com>
2024-03-04 13:02:15 +0000

    Footer

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,31 +1,59 @@
-
 <footer class="footer">
     <div class="container-footer">
         <div class="upper-footer">
-            <div class="logo"><img src="{{ asset('img/Logo BrunoPedro.png') }}" alt="logo branco pulse hub"></div>
-            <ul class="list-footer">
-                <li><a class="nav-hiper scrollto @if (Route::currentRouteName() == 'areaClinica') active @endif"
-                    href="{{ route('areaClinica') }}">Área Clínica</a></li>
-                <li><a class="nav-hiper scrollto @if (Route::currentRouteName() == 'guiaSaude') active @endif"
-                    href="{{ route('guiaSaude') }}">Guia Saúde</a></li>
-                <li><a class="nav-hiper scrollto @if (Route::currentRouteName() == 'galeria') active @endif"
-                     href="{{ route('galeria') }}">Galeria</a></li>
-                <li><a class="nav-hiper scrollto @if (Route::currentRouteName() == 'contactos') active @endif"
-                     href="{{ route('contactos') }}">Contactos</a></li>
-                <li><a class="nav-hiper scrollto @if (Route::currentRouteName() == 'termosCondicoes') active @endif"
-                    href="{{ route('termosCondicoes') }}">Termos e Condições</a></li>
 
+            <div class="esquerda">
+                <div class="logo"><img src="{{ asset('img/Logo BrunoPedro.png') }}" alt="logo branco pulse hub"></div>
+                <div class="social-footer">
+                    <a href="#"><img src="{{ asset('img/email-branco.svg') }}" alt="email icon"></a>
+                    <a href="#"><img src="{{ asset('img/insta-branco.svg') }}" alt="instagram icon"></a>
+                    <a href="#"><img src="{{ asset('img/facebook-branco.svg') }}" alt="facebook icon"></a>
+                </div>
+            </div>
 
-            </ul>
-            <div class="social-footer">
-                <a href="#"><img src="{{ asset('img/email-branco.svg') }}" alt="email icon"></a>
-                <a href="#"><img src="{{ asset('img/insta-branco.svg') }}" alt="instagram icon"></a>
-                <a href="#"><img src="{{ asset('img/facebook-branco.svg') }}" alt="facebook icon"></a>
+            <div class="meio">
+                <h5>Links uteis</h5>
+                <div class="divisorblocof">
+                    <div class="linhaf"></div>
+                </div>
+                <ul class="list-footer">
+                    <li><a class="nav-hiper scrollto @if (Route::currentRouteName() == 'areaClinica') active @endif"
+                            href="{{ route('areaClinica') }}">Área Clínica</a></li>
+                    <li><a class="nav-hiper scrollto @if (Route::currentRouteName() == 'guiaSaude') active @endif"
+                            href="{{ route('guiaSaude') }}">Guia Saúde</a></li>
+                    <li><a class="nav-hiper scrollto @if (Route::currentRouteName() == 'galeria') active @endif"
+                            href="{{ route('galeria') }}">Galeria</a></li>
+                    <li><a class="nav-hiper scrollto @if (Route::currentRouteName() == 'contactos') active @endif"
+                            href="{{ route('contactos') }}">Contactos</a></li>
+                    <li><a class="nav-hiper scrollto @if (Route::currentRouteName() == 'termosCondicoes') active @endif"
+                            href="{{ route('termosCondicoes') }}">Termos e Condições</a></li>
+                </ul>
+            </div>
+
+            <div class="direita">
+                <h5>Contactos</h5>
+
+                <div class="divisorblocof">
+                    <div class="linhaf"></div>
+                </div>
+
+                <div class="dirinfo">
+                    
+                    <i class="fas fa-phone" style="color: #9a89f4;"></i>
+                    <div class="info-contentf">
+                        <p>236 922 431</p>
+                    </div>
+
+                    <i class="fa-solid fa-envelope" style="color: #ffffff;"></i>
+                    <p>bruno_pedro.lda@hotmail.com</p>
+                </div>
+
             </div>
         </div>
         <img class="line" src="{{ asset('img/Line_2.svg') }}" alt="grey line footer">
         <div class="lower-footer">
+
             <p>Copyright © 2024 Clínica Dentária Bruno & Pedro, lda. Todos os direitos reservados.</p>
         </div>
-    </div>
+
 </footer>
