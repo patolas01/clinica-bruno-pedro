5f12d6931267c685cb27d58c36c857c6f09cc7ff
Ins1der2317 <150137722+Ins1der2317@users.noreply.github.com>
2024-03-06 16:11:19 +0000

    Fonte do site

--- resources/views/layouts/partials/navbar.blade.php
+++ resources/views/layouts/partials/navbar.blade.php
@@ -5,7 +5,7 @@
     rel="stylesheet">
 
 <!-- Custom styles for this template -->
-<link href="{{ asset('css/sb-admin-2.css') }}" rel="stylesheet">
+
 
 <!-- Custom styles for this page -->
 {{-- <link href="{{ asset('vendor/datatables/dataTables.bootstrap4.min.css') }}" rel="stylesheet"> --}}
@@ -181,20 +181,5 @@
 
 
 </nav>
-
-
-{{-- <!-- Bootstrap core JavaScript-->
-<script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
-<script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-
-<!-- Core plugin JavaScript-->
-<script src="{{ asset('vendor/jquery-easing/jquery.easing.min.js') }}"></script>
-
-<!-- Custom scripts for all pages-->
-<script src="{{ asset('vendor/jquery/sb-admin-2.min.js') }}"></script>
-
-<!-- Page level plugins -->
-<script src="{{ asset('vendor/datatables/jquery.dataTables.min.js') }}"></script>
-<script src="{{ asset('vendor/datatables/dataTables.bootstrap4.min.js') }}"></script> --}}
 
 @yield('scripts')
