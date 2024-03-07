9e6178085847ee367e59a75a6546c006535331c0
Ins1der2317 <150137722+Ins1der2317@users.noreply.github.com>
2024-03-07 16:09:01 +0000

    area clinica e equipa

--- resources/views/equipa.blade.php
+++ resources/views/equipa.blade.php
@@ -10,25 +10,35 @@
 
 
 
-        {{--
-        @if (!is_null($tiposplanos) && count($tiposplanos) > 0)
-            @foreach ($tiposplanos as $tipoplano)
-                @if (strtolower($tipoplano->classe) == 'normal')
-                 --}}
-
-
-        {{--
-                @endif
-            @endforeach
-        @else
-            <p>Nenhum tipo de plano disponível no momento.</p>
-        @endif
- --}}
 
  <div class="rowequipa">
 
     <div class="cardequipa">
         <img src="{{ asset('img/dentist-2589771.jpg') }}" alt="Musculação">
+        <div class="card-conteudo">
+        <h2>Card 1</h2>
+        <p>Conteúdo do Card 1</p>
+        </div>
+    </div>
+
+    <div class="cardequipa">
+        <img src="{{ asset('img/dentist-2589771.jpg') }}" alt="Musculação">
+        <div class="card-conteudo">
+        <h2>Card 1</h2>
+        <p>Conteúdo do Card 1</p>
+        </div>
+    </div>
+
+    <div class="cardequipa">
+        <img src="{{ asset('img/perfil.jpg') }}" alt="Musculação">
+        <div class="card-conteudo">
+        <h2>Card 1</h2>
+        <p>Conteúdo do Card 1</p>
+        </div>
+    </div>
+
+    <div class="cardequipa">
+        <img src="{{ asset('img/nagumo.jpg') }}" alt="Musculação">
         <div class="card-conteudo">
         <h2>Card 1</h2>
         <p>Conteúdo do Card 1</p>
