5c51a8ee07251e3a5ee2ce074c5b9c93680c11da
Ins1der2317 <150137722+Ins1der2317@users.noreply.github.com>
2024-03-18 12:55:54 +0000

    migração especialidade e detalhe especialidade

--- database/migrations/2024_03_18_122701_create_especialidade_table.php
+++ database/migrations/2024_03_18_122701_create_especialidade_table.php
@@ -14,6 +14,10 @@ return new class extends Migration
         Schema::create('especialidade', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('nome', 70);
+            $table->text('curt_desc');
+            $table->string('icon');
+            $table->softDeletes();
         });
     }
 
