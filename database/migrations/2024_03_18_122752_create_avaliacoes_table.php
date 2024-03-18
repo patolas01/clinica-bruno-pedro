81a24b03a5c57de995f17f4830337ebf0b575649
duarteferreira18 <2221030@my.ipleiria.pt>
2024-03-18 12:58:10 +0000

    avaliacoes migracoes

--- database/migrations/2024_03_18_122752_create_avaliacoes_table.php
+++ database/migrations/2024_03_18_122752_create_avaliacoes_table.php
@@ -14,6 +14,9 @@ return new class extends Migration
         Schema::create('avaliacoes', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->enum('classificacao', ['0', '1', '2', '3', '4', '5'])->default('0');
+            $table->string('texto',150);
+            $table->softDeletes();
         });
     }
 
