99f002cb3eeec8871461c1cd623de7ed65861467
Ins1der2317 <150137722+Ins1der2317@users.noreply.github.com>
2024-03-21 15:52:42 +0000

    detalhe esp

--- resources/views/_admin/detalhe-esp/edit.blade.php
+++ resources/views/_admin/detalhe-esp/edit.blade.php
@@ -1,4 +1,4 @@
-@extends('layout.admin')
+@extends('layouts.admin')
 
 @section('content')
 
@@ -6,18 +6,18 @@
 
      <div class="card shadow mb-4">
         <div class="card-header py-3">
-			Editar Tipo de Post
+			Editar Detalhe da especialidade
         </div>
         <div class="card-body">
 
-			<form method="POST" action="{{route('admin.tipopost.update', $tipopost)}}" class="form-group">
+			<form method="POST" action="{{route('detalhe-esp.update', $tipopost)}}" class="form-group">
                 @csrf
                 @method('PUT')
-				@include('_admin.tipopost.partials.add-edit')
+				@include('_admin.detalhe-esp.partials.add-edit')
 				<div class="form-group">
 					<button type="submit" class="btn btn-success" name="ok">Guardar</button>
 
-					<a href="{{route('admin.tipopost.index')}}" class="btn btn-default">Cancelar</a>
+					<a href="{{route('detalhe-esp.index')}}" class="btn btn-default">Cancelar</a>
 
 				</div>
 
