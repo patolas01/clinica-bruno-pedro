d45424b96819fcef52ecef4ba63c4d4b478702e9
Paulo André Leal <pauloandeleal@gmail.com>
2024-04-09 16:16:10 +0100

    drop fiox

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,13 +1,13 @@
-// const toogle_btn = document.querySelector('.toggle_btn');
-//     const close_btn = document.querySelector('.close_btn');
-//     const dropDownMenu = document.querySelector('.dropdown_menu');
+const toogle_btn = document.querySelector('.toggle_btn');
+const close_btn = document.querySelector('.close_btn');
+const dropDownMenu = document.querySelector('.dropdown_menu');
 
-//     toogle_btn.onclick = function () {
-//       dropDownMenu.classList.remove('open');
-//     }
-//     close_btn.onclick = function () {
-//       dropDownMenu.classList.toggle('open');
-//     }
+toogle_btn.onclick = function () {
+    dropDownMenu.classList.remove('open');
+}
+close_btn.onclick = function () {
+    dropDownMenu.classList.toggle('open');
+}
 
 
 
