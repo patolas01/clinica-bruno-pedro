1c19d4dd5629ddfbb923e5e507671eac8d9c11fe
Paulo André Leal <pauloandeleal@gmail.com>
2024-05-14 12:45:38 +0100

    sobre página fixed

--- resources/views/sobre.blade.php
+++ resources/views/sobre.blade.php
@@ -14,19 +14,34 @@
 
     <section id="main2">
         <div class="informacao about-us">
-            <p>A Clínica foi fundada em <strong>18 de setembro de 2005</strong> com o compromisso de proporcionar tratamentos de
-                <strong>qualidade</strong> na área da <strong>Medicina Oral</strong>. Cada paciente é recebido como um
-                amigo, e aqui levamos o sorriso muito a sério.</p>
+            <p>A Clínica foi fundada em <strong>18 de setembro de 2005</strong> com o compromisso de proporcionar
+                tratamentos de
+                <strong>qualidade</strong> na área da <strong>Medicina Oral</strong>. Cada paciente é recebido como
+                um
+                amigo, e aqui levamos o sorriso muito a sério.
+            </p>
 
-            <p>Oferecemos uma ampla gama de serviços, incluindo <strong>Dentisteria</strong>, <strong>Estética</strong>,
-                <strong>Endodontia</strong>, <strong>Higiene Oral</strong>, <strong>Periodontologia</strong>,
-                <strong>Prótese Fixa</strong>, <strong>Prótese Removível</strong>, <strong>Ortodontia</strong>,
-                <strong>Odontopediatria</strong>, <strong>Odontogeriatria</strong>, <strong>Cirurgia Oral</strong> e
-                <strong>Implantes</strong>.</p>
-
-            <p>Localizada na <strong>Rua Fonte da Vila, nº 922, 3100-012 Abiul</strong>, estamos sempre disponíveis para
-                atender às suas necessidades. Entre em contato connosco pelo telefone <strong>236 922 431</strong> para
-                agendar a sua consulta. Estamos aqui para cuidar do seu sorriso!</p>
+            <p>Oferecemos uma ampla gama de serviços, incluindo:
+            <ul>
+                <li><a href="">Dentisteria</a></li>
+                <li><a href="">Estética</a></li>
+                <li><a href="">Endodontia</a></li>
+                <li><a href="">Higiene Oral</a></li>
+                <li><a href="">Periodontologia</a></li>
+                <li><a href="">Prótese Fixa</a></li>
+                <li><a href="">Prótese Removível</a></li>
+                <li><a href="">Ortodontia</a></li>
+                <li><a href="">Odontopediatria</a></li>
+                <li><a href="">Odontogeriatria</a></li>
+                <li><a href="">Cirurgia
+                        Oral</a></li>
+                <li><a href="">Implantes</a></li>
+            </ul>
+            </p>
+            <p>Localizada na <a href="https://maps.app.goo.gl/QmeayfJtorh9GmiZ9" target="_blank">Rua Fonte da Vila, nº 922, 3100-012 Abiul</a>, estamos sempre disponíveis
+                para
+                atender às suas necessidades. Entre em contato connosco pelo telefone <a href="tel:236 922 431">236 922 431</a> para
+                agendar a sua consulta.<br> Estamos aqui para cuidar do seu sorriso!</p>
 
         </div>
     </section>
