16a8868d454f4513172d8742739a232b03a6720a
duarteferreira18 <2221030@my.ipleiria.pt>
2024-06-21 14:28:34 +0100

    codigo limpo

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -57,7 +56,7 @@ class User extends Authenticatable
     }
     public function isAdmin()
     {
-        return $this->perm === 'A'; // Considerando que 'A' representa o papel de administrador
+        return $this->perm === 'A';
     }
 
 }
